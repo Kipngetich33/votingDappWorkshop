94131e4865f90218453898a6fc2145d0bf8c08ff
Kipngetich33 <khalifngeno@gmail.com>
2022-09-27 20:00:50 +0300

    finish working on the voting Dapp workshop

--- index.mjs
+++ index.mjs
@@ -1,27 +1,121 @@
 import {loadStdlib} from '@reach-sh/stdlib';
 import * as backend from './build/index.main.mjs';
+import { ask, yesno, done } from '@reach-sh/stdlib/ask.mjs'
+
+// define global variables & consts
 const stdlib = loadStdlib(process.env);
-
 const startingBalance = stdlib.parseCurrency(100);
+let votingDone = false;
+const voters = [];
+let ctcVoteCordinator = null;
 
-const [ accAlice, accBob ] =
-  await stdlib.newTestAccounts(2, startingBalance);
-console.log('Hello, Alice and Bob!');
+// define helper functions
+const getVotingPeriod = async () => ask(
+  'Please enter a voting period',
+  (x => x) // use call back to return the details entered by the user
+)
 
-console.log('Launching...');
-const ctcAlice = accAlice.contract(backend);
-const ctcBob = accBob.contract(backend, ctcAlice.getInfo());
+const castVote = async () => await ask(
+  'Please Cast you vote below: 1 - Candidate 1, 2 - Candidate 2.',
+  (x => x) // use call back to return the details entered by the user
+)
 
-console.log('Starting backends...');
-await Promise.all([
-  backend.Alice(ctcAlice, {
-    ...stdlib.hasRandom,
-    // implement Alice's interact object here
-  }),
-  backend.Bob(ctcBob, {
-    ...stdlib.hasRandom,
-    // implement Bob's interact object here
-  }),
-]);
+const getContractFromVoter = async () => await ask(
+  'Enter the voting contract details to continue',
+  (x => x) // use call back to return the details entered by the user
+)
 
-console.log('Goodbye, Alice and Bob!');
+const defineUserRole = async () => await ask(
+  'Are you a vote Cordinator? (y/n)',
+  yesno
+)
+
+
+// start program section
+console.log("********************************************* Voting System ***************************************************")
+console.log("Starting Contract")
+
+// determine the users role
+const userRole =  await defineUserRole() ? "voteCordinator" : "voter"
+console.log(`You are participating as a ${userRole}`)
+
+// modify interaction based on role
+if(userRole == "voteCordinator" ){
+  // this is the vote cordinator and hence need to deploy the contract
+  const accVoteCordinator = await stdlib.newTestAccount(startingBalance);
+  console.log('Created account for VoteCordinator');
+  // allow accVoteCordinator to attach to the backend as the deployer of the contract
+  ctcVoteCordinator = accVoteCordinator.contract(backend);
+  // display contract details so that users can attach
+  ctcVoteCordinator.getInfo().then((contractDetails) => {
+  console.log(`Contract Details : ${JSON.stringify(contractDetails._hex)}`)
+  })
+
+  // crate a partcipant interface for the voter cordininator
+  await ctcVoteCordinator.participants.VoteCordinator({
+    votingPeriod: await getVotingPeriod(),
+    // votingPeriod:10,
+    votingReady: () => {
+      console.log("Voting is ready.")
+    },
+    voteStatus: (vote) => {
+      console.log(`User Vote ${vote}`);
+    }, 
+    finalVote: (candidate1, candidate2) => { 
+      let firstCandidateVotes = parseInt(candidate1) 
+      let secondCandidateVotes = parseInt(candidate2)   
+      // console the final results
+      console.log(`First Candidate: ${firstCandidateVotes}, Second Candidate: ${secondCandidateVotes}`);
+
+      if(firstCandidateVotes == secondCandidateVotes){
+        console.log('There was a Draw')
+      }else if(firstCandidateVotes > secondCandidateVotes){
+        console.log('First Candidate Wins')
+      }else{
+        console.log('Second Candidate Wins')
+      }
+    },
+   
+    terminateContract: async () => {
+      const defineUserRole = async () => await ask(
+        'Do you want to terminate contract?',
+        yesno
+      )
+      const terminate = await defineUserRole()
+      if(terminate){
+        console.log("Terminating...")
+        // mark voting as done
+        votingDone = true
+      }else{
+        console.log("Contract will keep running until the time elapses.")
+      }
+    }
+  })
+}else{
+  // add the Voter APIs here
+  console.log("You at participating as a Voter")
+  
+   // get user's vote
+   const castedVote = await castVote()
+   console.log("castedVote",castedVote)
+
+  // create a new test account for user
+  const acc = await stdlib.newTestAccount(startingBalance);
+  const constractDetails = getContractFromVoter()
+
+  // attach to the contract in the backend
+  const ctc = acc.contract(backend,constractDetails);
+  // call the voters API
+
+  try{
+    // now increment the vote here
+    const [ currentVote ] = await ctc.apis.Voter.vote(castedVote);
+    console.log(`Your latest vote is ${currentVote}`)
+  }catch (e) {
+    console.log(`Failed to Vote`)
+  } 
+}
+
+console.log('Thanks for Making you voice Heard');
+votingDone = true
+done()
--- index.rsh
+++ index.rsh
@@ -1,19 +1,79 @@
 'reach 0.1';
 
 export const main = Reach.App(() => {
-  const A = Participant('Alice', {
-    // Specify Alice's interact interface here
+
+  const VoteCordinator = Participant('VoteCordinator', {
+    votingPeriod:UInt,
+    getVoteDuration: Fun([],UInt),
+    votingReady: Fun([],Null),
+    finalVote: Fun([UInt,UInt],Null),
   });
-  const B = Participant('Bob', {
-    // Specify Bob's interact interface here
+  const Voter = API('Voter', {
+    vote: Fun([UInt],Tuple(UInt)),
   });
+
+  // now initialize the contract
   init();
-  // The first one to publish deploys the contract
-  A.publish();
+
+  // create a helper function for determing votes cast
+  const determineAdditionOfVotes = (vote,contestantIndex) => {
+    // contestant index 1 for Candidate 1 and 2 for Candidate 2
+    return vote == contestantIndex ? 1:0
+  }
+
+  // 1. The Vote Coordinator publishes the voting period
+  // first get the voting period from (VoteCordinator local step)
+  VoteCordinator.only(() => {
+    const ctcVotingPeriod = declassify(interact.votingPeriod)
+  })
+  VoteCordinator.publish(ctcVotingPeriod);
   commit();
-  // The second one to publish always attaches
-  B.publish();
+
+  
+  //2. While the voting period hasn't elapsed(use timeout and parallel reduce)
+
+  // publish something in order to enter into a concesus step for the loop
+  VoteCordinator.only(() => {
+  });
+
+  VoteCordinator.publish();
+  const end = lastConsensusTime() + ctcVotingPeriod;
+
+  const [ firstCandidateVotes, secondCandidateVotes ] = parallelReduce([ 0, 0 ])
+    .invariant(balance() >= 0 )
+    .while(lastConsensusTime() <= end )
+
+    //3. Voters publishes their votes to the smart contract through voter API
+    .api_(Voter.vote, (vote) => {
+
+      return [vote, (notify) => {
+        notify([vote]);
+        
+        return [
+          firstCandidateVotes +  determineAdditionOfVotes(vote,1),
+          secondCandidateVotes + determineAdditionOfVotes(vote,2),
+        ]
+        
+      }];
+    })
+    // .timeout(absoluteTime(end), () => {
+    .timeout(absoluteTime(end), () => {
+      VoteCordinator.publish();
+      return [firstCandidateVotes, secondCandidateVotes]
+    });
+
+  // transfer any remaining funds in the contact to the vote VoteCordinator' account
+  transfer(balance()).to(VoteCordinator);
+  // commit to go back to the context step here
   commit();
-  // write your program here
+
+  // 4. When the voting period elapses the contract determines the winner based on the number of cast votes
+  VoteCordinator.interact.finalVote(firstCandidateVotes,secondCandidateVotes)
+
+  // terminate the contract
   exit();
 });
+
+
+
+
